3c8097304c3d1858452090d6ad3612f7731e84a9
renetanikolova <reneta.y.nikolova@gmail.com>
2019-06-14 16:45:55 +0300

    init project

---
+++ src/main/java/com/optimizer/timeblockingapp/TimeBlockingAppApplication.java
@@ -0,0 +1,13 @@
+package com.optimizer.timeblockingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeBlockingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeBlockingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/optimizer/timeblockingapp/TimeBlockingAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.optimizer.timeblockingapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TimeBlockingAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
